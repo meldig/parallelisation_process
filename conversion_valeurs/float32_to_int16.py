f72ecee06e55238510e1fe0c12f1cea737652d5c
ClementAlba <93247842+ClementAlba@users.noreply.github.com>
2022-04-19 13:43:48 +0200

    Ajout de scripts (#1)
    Script de conversion d'un raster de Float32 vers Int16
    * usage de dask et rasterio
    * parallelisation dask array

---
+++ conversion_valeurs/float32_to_int16.py
@@ -0,0 +1,45 @@
+import dask.array as da
+import rasterio
+
+
+def multiply(array, data):
+    # Tableau de chunks pour le dask array
+    chunks = []
+    chunk = int(input("Entrez une valeur pour le tuple de chunks : "))
+
+    # Entrez -1 pour arrêter la boucle
+    while chunk != -1:
+        chunks.append(chunk)
+        chunk = int(input("Entrez une valeur pour le tuple de chunks : "))
+
+    # Création du dask array et transformation du tableau de chunks en tuple
+    daskArray = da.from_array(array, tuple(chunks))
+
+    # Si la valeur est >= au seuil on la multiplie par 10, sinon on la remplace par une valeur de nodata
+    seuil = int(input("Entrez la valeur du seuil pour la multiplication : "))
+    multipliedDaskArray = da.where(daskArray >= seuil, 10*daskArray, data['nodata'])
+    return multipliedDaskArray
+
+
+def createRaster(array, data):
+    # Conversion du type de tableau de float32 à rasterio.int16
+    convertedArray = array.astype(rasterio.int16)
+
+    # Création du nouveau raster
+    outputRaster = input("Entrez le chemin vers votre fichier de sortie raster : ")
+    with rasterio.open(outputRaster, 'w', **data) as dst:
+        dst.write(convertedArray)
+
+
+if __name__ == "__main__":
+    # Le chemin du raster en entée (sans guillemets)
+    rasterPath = input("Entrez le chemin de votre raster : ")
+
+    # Récupération des métadonnées du raster
+    with rasterio.open(rasterPath) as raster:
+        npArray = raster.read()
+        data = raster.profile
+        data['nodata'] = int(input("Entrez une valeur de nodata : "))
+
+    multipliedArray = multiply(npArray, data)
+    createRaster(multipliedArray, data)
